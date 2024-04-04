bfd003b1f054b66fbf26035b87d9e3178281a9e9
thienphu <phutot1111@gmail.com>
2024-04-04 16:08:23 +0700

    them chuc nang dang xuat

--- screen/home/SettingScreen.js
+++ screen/home/SettingScreen.js
@@ -10,12 +10,28 @@ import { BLUE, GRAY, WHITE } from "../colors/Colors";
 import { ScrollView } from "react-native-virtualized-view";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { ListItem } from "react-native-elements";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function SettingScreen({ navigation }) {
 
   const [options, setOptions] = React.useState([
-    { title: "Đăng xuất", icon: <Icon name="logout" size={30} color={"red"} /> }
+    { title: "Đăng xuất", icon: <Icon name="logout" size={30} color={"red"} />, click: logoutUser }
   ])
+
+  function logoutUser() {
+
+    const removeToken = async () => {
+      try {
+        await AsyncStorage.removeItem("tokenRegister");
+        alert("Đăng xuất thành công")
+        navigation.push("LoginAndSignIn")
+      } catch (e) {
+        alert("Đăng xuất thất bại")
+      }
+    }
+
+    removeToken()
+  }
 
   return (
     <View style={styles.container}>
@@ -26,6 +42,7 @@ function SettingScreen({ navigation }) {
           options.map((option, index) => {
             return (
               <ListItem
+                onPress={() => { option.click() }}
                 key={index}
                 style={{
                   width: "100%",
