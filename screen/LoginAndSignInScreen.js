b410772dc84d58efd60691db5e8820104378eff0
thienphu <phutot1111@gmail.com>
2024-04-04 15:53:32 +0700

    kiem tra tai hien tai co token de dang nhap khong  neu co thi vao man hinh chinh luon neu khong thi o lai

--- screen/LoginAndSignInScreen.js
+++ screen/LoginAndSignInScreen.js
@@ -3,53 +3,80 @@ import ZaloImage from "../images/zalo_icon.png"
 import { Image, StyleSheet, View } from 'react-native'
 import { Button } from 'react-native-elements'
 import { BLUE, GRAY } from './colors/Colors'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function LoginAndSignInScreen({ navigation }) {
 
-    return (
-        <View style={styles.container}>
-            <Image
-                source={ZaloImage}
-                style={{ width: 150, height: 150 }}
-            />
-            <View>
-                <Button
-                    title="Đăng nhập"
-                    buttonStyle={{
-                        backgroundColor: BLUE,
-                        borderWidth: 2,
-                        borderColor: 'white',
-                        borderRadius: 30,
-                        height: 50,
-                        width: 320
-                    }}
-                    containerStyle={{
-                        marginHorizontal: 50,
-                        marginVertical: 10,
-                    }}
-                    titleStyle={{ fontSize: 14 }}
-                    onPress={() => { navigation.push("Login") }}
+    const [isUser, setIsUser] = React.useState(false)
+
+    React.useEffect(() => {
+        const getData = async () => {
+            try {
+                const value = await AsyncStorage.getItem('tokenRegister');
+                if (value !== null) {
+                    setIsUser(true)
+                }
+            } catch (e) {
+                console.error(e)
+            }
+        };
+
+        getData()
+    }, [])
+
+    React.useEffect(() => {
+        if (isUser === true) {
+            navigation.push("Index");
+        }
+    }, [isUser]);
+
+    if (isUser) {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={ZaloImage}
+                    style={{ width: 150, height: 150 }}
                 />
-                <Button
-                    title="Tạo tài khoản mới"
-                    buttonStyle={{
-                        backgroundColor: GRAY,
-                        borderWidth: 2,
-                        borderColor: 'white',
-                        borderRadius: 30,
-                        height: 50,
-                        width: 320
-                    }}
-                    containerStyle={{
-                        marginHorizontal: 50,
-                        marginVertical: 10,
-                    }}
-                    titleStyle={{ fontSize: 14, color: "black" }}
-                    onPress={() => { navigation.push("SignIn1") }}
-                />
+                <View>
+                    <Button
+                        title="Đăng nhập"
+                        buttonStyle={{
+                            backgroundColor: BLUE,
+                            borderWidth: 2,
+                            borderColor: 'white',
+                            borderRadius: 30,
+                            height: 50,
+                            width: 320
+                        }}
+                        containerStyle={{
+                            marginHorizontal: 50,
+                            marginVertical: 10,
+                        }}
+                        titleStyle={{ fontSize: 14 }}
+                        onPress={() => { navigation.push("Login") }}
+                    />
+                    <Button
+                        title="Tạo tài khoản mới"
+                        buttonStyle={{
+                            backgroundColor: GRAY,
+                            borderWidth: 2,
+                            borderColor: 'white',
+                            borderRadius: 30,
+                            height: 50,
+                            width: 320
+                        }}
+                        containerStyle={{
+                            marginHorizontal: 50,
+                            marginVertical: 10,
+                        }}
+                        titleStyle={{ fontSize: 14, color: "black" }}
+                        onPress={() => { navigation.push("SignIn1") }}
+                    />
+                </View>
             </View>
-        </View>
-    )
+        )
+    }
+
 }
 
 const styles = StyleSheet.create({
