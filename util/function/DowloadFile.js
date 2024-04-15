68547d85e07cea6c9c2c4c74e8a3bc6dd68d454f
thienphu <phutot1111@gmail.com>
2024-04-15 23:43:25 +0700

    xoa mot so depencies khong can thiet--day la phien ban dang on dinh

--- util/function/DowloadFile.js
+++ util/function/DowloadFile.js
@@ -1,22 +1,10 @@
-
-
 const getFileNameFromUri = (uri) => {
-    // Tách uri thành mảng các phần bằng dấu /
     const parts = uri.split('/');
-    // Lấy phần cuối cùng của mảng, đó chính là tên tệp
     const fileName = parts[parts.length - 1];
     return fileName;
 };
 
 
-const downloadFileFromUri = async (uri) => {
-    const fileName = getFileNameFromUri(uri);
-    return await downloadFileToDownloads(uri, fileName);
-};
-
-
-
 export {
     getFileNameFromUri,
-    downloadFileFromUri
 } 
